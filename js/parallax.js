a64cbbf24d8475a04f94d5cdcdf456f2558406f6
gabrielvicentealva <gabrielvicentealva@gmail.com>
2023-01-31 22:39:58 -0500

    vista móvil

--- js/parallax.js
+++ js/parallax.js
@@ -1,12 +1,15 @@
 var r = document.querySelector(':root');
 let enc = document.getElementById('enc');
-window.addEventListener('scroll', parallax);
 
+window.addEventListener('scroll', parallax);
 window.addEventListener("resize", parallax);
+document.body.addEventListener('touchmove', parallax);
 
 function parallax(event) {
     let { scrollY } = window;
-	let parY = (enc.offsetHeight - 80)*(1-scrollY/enc.offsetHeight)
+	// console.log(scrollY + ' ' + document.documentElement.scrollHeight);
+	console.log(enc.offsetHeight);
+	let parY = (1.2*enc.offsetHeight - 115)*(1-scrollY/enc.offsetHeight)
 	r.style.setProperty('--parallax-y', parY+'px');
 }
 parallax();
